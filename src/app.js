b0a9d0e3d4ee84df480b7334d6079852bf06e4ca
Ivan Nedelchev <130435484+ivan-nedelchev@users.noreply.github.com>
2023-12-15 00:17:27 +0200

    .

--- src/app.js
+++ src/app.js
@@ -1,16 +1,20 @@
 import {page, html, render} from './util.js'
+import { showCreate } from './views/create.js';
 import { showIndex } from './views/index.js'
-import { showLogin } from './views/login.js'
+import {showLogin } from './views/login.js'
+import { showRegister } from './views/register.js';
+let divContainer = document.querySelector("div.container");
 
 page(addToContext)
 page('/login', showLogin)
 page('/', showIndex)
-page.start()
+page('/create', showCreate)
+page('/register', showRegister)
 
+
+page.start()
 function addToContext(ctx, next) {
-    ctx.page = page;
-    ctx.html = html;
-    ctx.render = render;
-    ctx.wtf = "wtf"
+    let wtf = "wtf"
+    Object.assign(ctx, {page, html, render, wtf, divContainer})
     next()
 }
